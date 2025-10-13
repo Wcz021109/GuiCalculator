f625f491c8a788687c2430cd0d805d401f469c3e
Wcz021109 <134478120+Wcz021109@users.noreply.github.com>
2025-10-13 18:17:08 +0800

    Initial Commit

--- src/calculate_core/CalculateCore.cpp
+++ src/calculate_core/CalculateCore.cpp
@@ -1,1 +1,76 @@
 #include "CalculateCore.h"
+
+#include <QStack>
+#include <QMap>
+#include <QDebug>
+
+CalculateCore::CalculateCore(QObject *parent)
+    : QObject(parent){
+    //  初始化代码
+}
+
+CalculateCore::~CalculateCore() {
+
+}
+
+QString CalculateCore::formula() const{
+    return m_formula;
+}
+
+void CalculateCore::setFormula(const QString &value) {
+    if (m_formula != value) {
+        m_formula = value;
+        emit formulaChanged();
+    }
+}
+void CalculateCore::clear() {
+    m_formula = "#";
+    result = 0.0;
+    emit formulaChanged();
+}
+void CalculateCore::appendToFormula(const QChar &input){
+    m_formula.append(input);
+    emit formulaChanged();
+}
+void CalculateCore::chopFromFormula() {
+    if (m_formula.length() > 1) {
+        m_formula.chop(1);
+        emit formulaChanged();
+    }else {
+        qCritical() << "formula is empty";
+    }
+}
+
+int CalculateCore::getPriority(const QChar &op1, const QChar &op2) const {
+    static QMap<QChar, QMap<QChar,QChar>> opMap{
+        {'+',{{'+','>'},{'-','>'},{'*','<'},{'/','<'},{'^','<'},{'(','<'},{')','>'},{'#','>'}}},
+        {'-',{{'+','>'},{'-','>'},{'*','<'},{'/','<'},{'^','<'},{'(','<'},{')','>'},{'#','>'}}},
+        {'*',{{'+','>'},{'-','>'},{'*','>'},{'/','>'},{'^','<'},{'(','<'},{')','>'},{'#','>'}}},
+        {'/',{{'+','>'},{'-','>'},{'*','>'},{'/','>'},{'^','<'},{'(','<'},{')','>'},{'#','>'}}},
+        {'^',{{'+','>'},{'-','>'},{'*','>'},{'/','>'},{'^','>'},{'(','<'},{')','>'},{'#','>'}}},
+        {'(',{{'+','>'},{'-','>'},{'*','>'},{'/','>'},{'^','>'},{'(','>'},{')','='},{'#','!'}}},
+        {')',{{'+','>'},{'-','>'},{'*','>'},{'/','>'},{'^','>'},{'(','!'},{')','>'},{'#','>'}}},
+        {'#',{{'+','<'},{'-','<'},{'*','<'},{'/','<'},{'^','<'},{'(','<'},{')','!'},{'#','='}}}
+    };
+    QChar relation=opMap[op1][op2];
+    switch (relation.unicode()) {
+        case '>' : return 2;break;
+        case '=': return 0;break;
+        case '<': return 1;break;
+        case '!': return -1;break;
+        default: return -2;break;
+    }
+}
+void CalculateCore::performOperation(const QString &num1, const QString &num2,const QChar &op) const{}
+bool CalculateCore::isOperator(const QChar &ch) const {
+    if (ch=="+"||ch=="-"||ch=="*"||ch=="/"||ch=="^"||ch=="("||ch==")"||ch=="#") return true;
+    else return false;
+}
+bool CalculateCore::isDigit(const QChar &ch) const {
+    return false;
+}
+
+void CalculateCore::calculate() {
+    QStack<QChar> OPND;
+    QStack<qreal> OPTR;
+}
--- src/calculate_core/CalculateCore.h
+++ src/calculate_core/CalculateCore.h
@@ -1,8 +1,43 @@
 #ifndef GUICALCULATOR_CALCULATECORE_H
 #define GUICALCULATOR_CALCULATECORE_H
 
+#include "Memory.h"
 
-class CalculateCore {
+#include <QObject>
+#include <QString>
+#include <QStringList>
+
+class CalculateCore : public QObject{
+    Q_OBJECT
+    Q_PROPERTY(QString formula READ formula WRITE setFormula NOTIFY formulaChanged)
+    Q_PROPERTY(QString memory READ memory WRITE memoryStored NOTIFY memoryChanged)
+public:
+    explicit CalculateCore(QObject *parent = nullptr);
+    ~CalculateCore();
+
+    QString formula() const;
+
+public slots:
+    Q_INVOKABLE void setFormula(const QString &value);
+    Q_INVOKABLE void clear();
+    Q_INVOKABLE void appendToFormula(const QChar &input);
+    Q_INVOKABLE void chopFromFormula();
+
+    Q_INVOKABLE void calculate();
+
+private:
+    QString m_formula = "#";
+    qreal result = 0.0;
+
+    int getPriority(const QChar &op1, const QChar &op2) const;
+    void performOperation(const QString &num1, const QString &num2,const QChar &op) const;
+    bool isOperator(const QChar &ch) const;
+    bool isDigit(const QChar &ch) const;
+
+signals:
+    void formulaChanged();
+    void calculateCompleted();
+    void errorOccurred();
 };
 
 
